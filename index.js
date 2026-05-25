2a9940e84860cf24d821b195d010e1f35941bc47
Ranul <pawar01214@gmail.com>
2026-05-25 15:06:58 +0530

    complete js file with ISBN NO. of book

--- index.js
+++ index.js
@@ -43,17 +43,17 @@ app.get("/", async (req, res) => {
 
 // Add New Book Route
 app.post("/add", async (req, res) => {
-  // Form se aane wala data destructure kar rahe hain
+  
   const { title, author, rating, date_read, isbn, notes } = req.body;
 
   try {
-    // Database me data INSERT karne ki SQL query
+   
     await db.query(
-      "INSERT INTO books (title, author, rating, date_read, isbn, notes) VALUES ($1, $2, $3, $4, $5, $6)",
+      "INSERT INTO book (title, author, rating, date_read, isbn, notes) VALUES ($1, $2, $3, $4, $5, $6)",
       [title, author, rating, date_read, isbn, notes]
     );
     
-    // Data save hone ke baad wapas home page par bhej do
+    
     res.redirect("/");
   } catch (error) {
     console.log("Error inserting data:", error);
@@ -61,6 +61,38 @@ app.post("/add", async (req, res) => {
   }
 });
 
+
+app.post("/delete", async (req, res) => {
+  const deleteId = req.body.deleteItemId; 
+
+  try {
+    
+    await db.query("DELETE FROM books WHERE id = $1", [deleteId]);
+    res.redirect("/"); 
+  } catch (error) {
+    console.log("Error deleting data:", error);
+    res.status(500).send("Error deleting the book.");
+  }
+});
+
+// Edit/Update Book Route
+app.post("/edit", async (req, res) => {
+  const id = req.body.updateItemId;
+  const newRating = req.body.updatedRating;
+  const newNotes = req.body.updatedNotes;
+
+  try {
+    
+    await db.query(
+      "UPDATE book SET rating = $1, notes = $2 WHERE id = $3",
+      [newRating, newNotes, id]
+    );
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error updating data:", error);
+    res.status(500).send("Error updating the book.");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
